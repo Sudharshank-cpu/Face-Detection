e19a38b047b21c407822f0254372184e38a3459f
Sudharshan Kumaresan <138243791+Sudharshank-cpu@users.noreply.github.com>
2024-09-30 10:52:06 +0530

    Update to make Choices to User

--- DetectMyFace.py
+++ DetectMyFace.py
@@ -2,8 +2,12 @@ import cv2
 
 face_cascade = cv2.CascadeClassifier('HaarcascadeFrontalFaceDefault.xml')  # Loads a Cascade Coordinates
 
-cap = cv2.VideoCapture(0)  # To Capture from Webcam
-# cap = cv2.VideoCapture('filename.mp4')  # To use Video file as Input
+choice = input("Which one do you use? (1)WebCam (2)Video File: ")    # Provides choice to user
+if choice in ["1", "WebCam"]:
+    cap = cv2.VideoCapture(0)  # To Capture from Webcam
+elif choice in ["2", "Video"]:
+    fileName = input("Enter Video File Name(with Location, If not Near this Script): ")
+    cap = cv2.VideoCapture('{}'.format(fileName))  # To use Video file as Input
 
 if not cap.isOpened():
     print("Input Error: Webcam or video file not found")
@@ -14,11 +18,9 @@ while True:
     faces = face_cascade.detectMultiScale(gray, 1.1, 4)  # Detect Faces
     for (x, y, w, h) in faces:  # Draw Rectangle around Each Face
         cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-
     cv2.imshow('img', img)  # Display
-    k = cv2.waitKey(30) & 0xff  # Stop if Escape Key (ASCII Value: 27) is Pressed
+    k = cv2.waitKey(30) & 0xff  # Stop if Escape Key(ASCII Value: 27) is Pressed
     if k == 27:
         break
-
 cap.release()  # Release Video Capture Object
 cv2.destroyAllWindows()  # Close all OpenCV windows
